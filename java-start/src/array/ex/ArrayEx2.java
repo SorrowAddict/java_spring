67ac38b9d45c7e553c48465a66e155e84ef07438
SorrowAddict <sonatine444@gmail.com>
2025-01-20 00:29:04 +0900

    feat: Ex) 배열의 입력과 출력

--- java-start/src/array/ex/ArrayEx2.java
+++ java-start/src/array/ex/ArrayEx2.java
@@ -3,6 +3,7 @@ package array.ex;
 import java.util.Scanner;
 
 public class ArrayEx2 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[5];
