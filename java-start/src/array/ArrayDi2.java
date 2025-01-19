0d0a19772d91c19e808de0592de23bc996dd8e1c
SorrowAddict <sonatine444@gmail.com>
2025-01-20 00:03:00 +0900

    feat: 2차원 배열 리팩토링

--- java-start/src/array/ArrayDi2.java
+++ java-start/src/array/ArrayDi2.java
@@ -3,14 +3,10 @@ package array;
 public class ArrayDi2 {
     public static void main(String[] args) {
         // 2x3 2차원 배열을 만든다.
-        int[][] arr = new int[2][3];    // 2행 3열
-
-        arr[0][0] = 1;
-        arr[0][1] = 2;
-        arr[0][2] = 3;
-        arr[1][0] = 4;
-        arr[1][1] = 5;
-        arr[1][2] = 6;
+        int[][] arr = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6}
+        };    // 2행 3열
 
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
