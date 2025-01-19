3c9c766b7a23cffc25c32ef1543f2a70ad33a006
SorrowAddict <sonatine444@gmail.com>
2025-01-19 23:52:20 +0900

    feat: 배열 리팩토링

--- java-start/src/array/Array1Ref2.java
+++ java-start/src/array/Array1Ref2.java
@@ -2,8 +2,7 @@ package array;
 
 public class Array1Ref2 {
     public static void main(String[] args) {
-        int[] students; // 배열 변수 선언
-        students = new int[]{90, 80, 70, 60, 50};   // 배열 생성과 초기화
+        int students[] = new int[]{90, 80, 70, 60, 50};   // 배열 생성과 초기화
 
         // 변수 값 사용
         for (int i = 0; i < students.length; i++) {
