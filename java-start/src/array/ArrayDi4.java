8a317aa8bd14a9717dda940220aee33867cafc48
SorrowAddict <sonatine444@gmail.com>
2025-01-20 00:07:05 +0900

    feat: 2차원 배열 리팩토링

--- java-start/src/array/ArrayDi4.java
+++ java-start/src/array/ArrayDi4.java
@@ -6,12 +6,14 @@ public class ArrayDi4 {
         int[][] arr = new int[3][3];    // 3행 3열
 
         int i = 1;
+        // 순서대로 1씩 증가하는 값을 입력한다.
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
                 arr[row][col] = i++;
             }
         }
-
+        
+        // 2차원 배열의 길이를 활용
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
                 System.out.print(arr[row][col] + " ");
