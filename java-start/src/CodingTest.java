faa49ad813facc1007f148aa1e3d106c5c168aca
SorrowAddict <sonatine444@gmail.com>
2025-01-23 23:41:59 +0900

    feat: Add CodingTest.java

--- java-start/src/CodingTest.java
+++ java-start/src/CodingTest.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CodingTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int A = sc.nextInt();
         int B = sc.nextInt();
         System.out.println(A + B);
