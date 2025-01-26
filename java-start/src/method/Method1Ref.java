a80d1e95d49a3cb8b581f36cf3268e5c74cfb01b
SorrowAddict <sonatine444@gmail.com>
2025-01-26 23:46:24 +0900

    feat: 메서드

---
+++ java-start/src/method/Method1Ref.java
@@ -0,0 +1,30 @@
+package method;
+
+public class Method1Ref {
+    public static void main(String[] args) {
+        // 변수만 다를 뿐 같은 연산을 반복할 경우 메서드를 이용할 수 있다.
+
+        // 계산 1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + "+" + b + " 연산 수행");
+        int sum1 = a + b;
+        System.out.println("결과1 출력: " + sum1);
+
+        System.out.println("=====================");
+
+        // 계산2
+        int x = 10;
+        int y = 20;
+        System.out.println(x + "+" + y + " 연산 수행");
+        int sum2 = x + y;
+        System.out.println("결과2 출력: " + sum2);
+    }
+
+    // add 메서드
+    public static int add(int a, int b) {
+        System.out.println(a + "+" + b + " 연산 수행");
+        int sum = a + b;
+        return sum;
+    }
+}
