eade60c6ff7add3e23e155eea1240aafa1cfb739
SorrowAddict <sonatine444@gmail.com>
2025-01-27 23:53:19 +0900

    feat: 메서드

--- java-start/src/method/Method1Ref.java
+++ java-start/src/method/Method1Ref.java
@@ -5,19 +5,11 @@ public class Method1Ref {
         // 변수만 다를 뿐 같은 연산을 반복할 경우 메서드를 이용할 수 있다.
 
         // 계산 1
-        int a = 1;
-        int b = 2;
-        System.out.println(a + "+" + b + " 연산 수행");
-        int sum1 = a + b;
+        int sum1 = add(5, 10);
         System.out.println("결과1 출력: " + sum1);
 
-        System.out.println("=====================");
-
         // 계산2
-        int x = 10;
-        int y = 20;
-        System.out.println(x + "+" + y + " 연산 수행");
-        int sum2 = x + y;
+        int sum2 = add(15, 20);
         System.out.println("결과2 출력: " + sum2);
     }
 
